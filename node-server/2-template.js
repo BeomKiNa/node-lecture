60008bf975850a522e8cb67f4ed85c8488937c0e
nabeomki <beomki69@gmail.com>
2022-01-21 20:58:13 +0900

    feat: Add ejs

---
+++ node-server/2-template.js
@@ -0,0 +1,44 @@
+const http = require("http");
+const fs = require("fs");
+const ejs = require("ejs");
+
+const PORT = 8080;
+const name = "Ki";
+const list = [
+  {
+    name: "HTML",
+  },
+  {
+    name: "CSS",
+  },
+  {
+    name: "JS",
+  },
+
+  {
+    name: "NODE",
+  },
+];
+
+const server = http.createServer((req, res) => {
+  const { url } = req;
+  res.setHeader("Content-Type", "text/html");
+
+  if (url === "/") {
+    ejs
+      .renderFile("./template/home.ejs", { name })
+      .then((data) => res.end(data));
+  } else if (url === "/detail") {
+    ejs
+      .renderFile("./template/detail.ejs", { list })
+      .then((data) => res.end(data));
+  } else {
+    ejs
+      .renderFile("./template/404.ejs", { name })
+      .then((data) => res.end(data));
+  }
+});
+
+server.listen(PORT, () =>
+  console.log(`🚀 Server listening: http://localhost:${PORT}`)
+);
