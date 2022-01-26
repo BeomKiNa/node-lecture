44eca5103212187968abb6b63dbc7a50a05aedb1
nabeomki <beomki69@gmail.com>
2022-01-26 22:49:58 +0900

    feat: 미들웨어

--- express/app.js
+++ express/app.js
@@ -3,8 +3,41 @@ import express from "express";
 const app = express();
 const PORT = 8080;
 
+app.all("/api", (req, res, next) => {
+  // 딱 작성된 주소와 일치하는 경우에만 작동
+  console.log("all");
+  next();
+});
+
+app.use("/sky", (req, res, next) => {
+  // 작성된 주소 이하의 어떤 경로에서도 작동
+  console.log("use");
+  next();
+});
+
+app.get(
+  "/",
+  (req, res, next) => {
+    console.log("first");
+    // next(new Error("error"));
+    res.send("hi");
+  },
+  (req, res, next) => {
+    console.log("first2");
+  }
+);
+
 app.get("/", (req, res, next) => {
-  res.send("Hello!");
+  console.log("second");
+});
+
+app.use((req, res, next) => {
+  res.status(404).send("Not found!");
+});
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  res.status(500).send("Try later");
 });
 
 app.listen(PORT, () =>
