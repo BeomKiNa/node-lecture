65ec53ac8938a20efc80b7b4bba41ea5f5384d07
nabeomki <beomki69@gmail.com>
2022-02-02 23:07:50 +0900

    feat: 내부 미들웨어들

--- express/app-route.js
+++ express/app-route.js
@@ -5,7 +5,9 @@ import userRouter from "./router/user.js";
 const app = express();
 const PORT = 8080;
 
-app.use(express.json());
+app.use(express.json()); // REST API -> Body
+app.use(express.urlencoded({ extended: false })); // HTML Form -> Body
+// app.use(express.static('path'));
 
 app.use("/posts", postRouter);
 app.use("/users", userRouter);
