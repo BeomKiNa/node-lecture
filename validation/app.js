3e598fef8e6c9500246accabbc2153eae8ac05b1
nabeomki <beomki69@gmail.com>
2022-03-28 22:47:21 +0900

    refactor: Sanitization

--- validation/app.js
+++ validation/app.js
@@ -17,12 +17,11 @@ app.post(
   "/users",
   [
     body("name")
-      .notEmpty()
-      .withMessage("이름을 입력하세요.")
+      .trim()
       .isLength({ min: 2, max: 10 })
       .withMessage("이름은 두 글자 이상, 열 글자 이하입니다"),
     body("age").notEmpty().isInt().withMessage("숫자를 입력하세요"),
-    body("email").isEmail().withMessage("이메일을 입력하시오"),
+    body("email").isEmail().withMessage("이메일을 입력하시오").normalizeEmail(),
     body("job.name").notEmpty(),
     validate,
   ],
